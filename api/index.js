eec0b9de9e5a2a91a458b304bd0bfa86c6215413
mophatc <mophatchemiati@gmail.com>
2024-08-10 10:17:26 +0300

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+  ? console.log("connected")
+  : console.log("failed");
 
 const app = express();
-const PORT=3000;
+const PORT = 3000;
 
-app.listen(PORT, ()=>{
-
-    console.log('Server is running on port 3000!!')
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000!!");
 });
