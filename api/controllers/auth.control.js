e4edb60cd94ee5a52bf0455c2e0cd840dd26f68f
mophatc <mophatchemiati@gmail.com>
2024-08-12 15:20:22 +0300

    Complete sign up functionality

--- api/controllers/auth.control.js
+++ api/controllers/auth.control.js
@@ -4,6 +4,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const signUp =  async (req, res, next) => {
 const { username, email, password }  = req.body;
+console.log(req.body);
 const hashedPassword = bcryptjs.hashSync(password,10);
 const newUser = new User({username, email, password : hashedPassword});
 
@@ -13,7 +14,7 @@ try {
     res.status(200).json('User Created successfully');
     
 } catch (error) {
-    next(errorHandler(550, 'error from function '));
+    next(errorHandler(550, 'Server error please try later'));
 
 }
 
--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,22 +1,84 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import {  Link, useNavigate } from "react-router-dom";
+
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null)
+  const[loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success===false){
+        setLoading(false);
+        setError(data.message);
+        return; 
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    
+  };
+
   return (
-    <div className='p-3  max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign UP</h1>
-      <form className='flex flex-col gap-3 '>
-        <input type="text" placeholder='Username' id="username"  className='border p-3 rounded-lg'/>
-        <input type="email" placeholder='Email' id="email"  className='border p-3 rounded-lg'/>
-        <input type="password" placeholder='Enter Password' id="password"  className='border p-3 rounded-lg'/>
-        <button  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70'>Sign UP</button>
+    <div className="p-3  max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign UP</h1>
+      <form className="flex flex-col gap-3 " onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          id="password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70">
+          {loading ? 'Loading...':'Sign Up'}
+        </button>
       </form>
-      <div className='mt-4 flex gap-2'>
-        <p>Have an Account ? </p> <Link to="/sign-in">
-        <span className='capitalize text-blue-600 font-bold'>sign in</span></Link>
+      <div className="mt-4 flex gap-2">
+        <p>Have an Account ? </p>{" "}
+        <Link to="/sign-in">
+          <span className="capitalize text-blue-600 font-bold">sign in</span>
+        </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
