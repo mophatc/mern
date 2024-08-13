2b72d751addf481d996ae91d3f1972aecabe95fc
mophatc <mophatchemiati@gmail.com>
2024-08-13 15:44:19 +0300

    Complete sign in page functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-function Signin() {
+function signIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [eye, setEye] = useState(false);
+  const [eyeclosed, setEyeClosed] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+    
+      navigate("/");
+      console.log(data)
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  const handleEye = () => {
+    setEye(!eye);
+    setEyeClosed(!eyeclosed);
+  };
+
   return (
-    <div>
-      Signin
+    <div className="p-3  max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-3 " onSubmit={handleSubmit}>
+        
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        
+
+<div className="  bg-white flex items-center cursor-pointer">
+<input
+          type={eye ? "password" : "text"}
+          placeholder="Enter Password"
+          id="password"
+          className=" p-3 rounded-lg w-full"
+          onChange={handleChange}
+        />
+        {eye ? <FaEye className="m-3  font-semibold  " onClick={handleEye}/> : <FaEyeSlash className="m-3  font-semibold  " onClick={handleEye}/>}
+        
+
+          </div>
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="mt-4 flex gap-2">
+        <p>Don't have an Account ? </p>{" "}
+        <Link to="/sign-up">
+          <span className="capitalize text-blue-600 font-bold">sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
 
-export default Signin
+export default signIn;
