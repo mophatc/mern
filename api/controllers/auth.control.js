6217d22281f8ad31923abe66eba87ae1cb2e74a1
mophatc <mophatchemiati@gmail.com>
2024-08-13 10:40:04 +0300

    Create Sign in API route

--- api/controllers/auth.control.js
+++ api/controllers/auth.control.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 
 export const signUp =  async (req, res, next) => {
 const { username, email, password }  = req.body;
@@ -14,8 +16,33 @@ try {
     res.status(200).json('User Created successfully');
     
 } catch (error) {
-    next(errorHandler(550, 'Server error please try later'));
+    next(error);
 
 }
 
 };
+
+
+export const signin = async (req, res, next)=>{
+    const { email, password } = req.body;
+
+    try {
+        
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong Credentials'));
+        const token = jwt.sign({id:validUser._id }, process.env.JWT_WEBTOKEN);
+        const {password:remainP, ...userData } = validUser._doc
+        res.cookie('mophatToken', token, {httpOnly:true}).status(200).json({
+            userData
+        })
+        
+        
+    } catch (error) {
+        next(error)
+        
+    }
+    
+
+}
