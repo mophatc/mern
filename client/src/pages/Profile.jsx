10a4b0877e7709b94f1043339df003bffc1b0fb3
mophatc <mophatchemiati@gmail.com>
2024-08-24 21:45:05 +0300

    Complete image functinality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ import { app } from '../firebase'
 export default function Profile() {
   const fileRef = useRef(null)
   const [fileUploadError, setFileUploadError] = useState(false)
-  const [filePer, setFilePerc] = useState(0)
+  const [filePerc, setFilePerc] = useState(0)
   const [formData, setFormData] = useState({})
   const {currentUser} = useSelector(state=>state.user)
   const [file, setFile ] = useState(undefined)
@@ -53,8 +53,22 @@ console.log(formData)
         <input type="file" onChange={(e)=>setFile(e.target.files[0])} hidden accept='image/*'  ref={fileRef} />
 
 
-      <img onClick={()=>{fileRef.current.click()}} src={currentUser.avatar} alt="profile" className='rounded-full w-20 object-cover cursor-pointer h-20 self-center '/>
+      <img onClick={()=>{fileRef.current.click()}} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full w-20 object-cover cursor-pointer h-20 self-center '/>
+      <p className='text-sm self-center'>
+        {fileUploadError ? (
+      <span className='text-red-700'>Error Occured while Uploading the Image(image must be less than 3mb and it must be an image file )</span>  )
 
+      : filePerc > 0 && filePerc<100 ? (
+        <span className='text-slate-500'> Uploading {filePerc} % </span>)
+        :
+        filePerc===100?(
+          <span className='text-green-500'>Image successfully Uploaded </span>
+        )
+        :(
+        ''
+
+      )}
+      </p>
     <input type="text"  id="username" placeholder='Username' className='mt-3 border p-3 rounded-lg' />
     <input type="email"  id="email" placeholder='Email' className='mt-3 border p-3 rounded-lg' />
     <input type="password"  id="password" placeholder='Password' className='mt-3 border p-3 rounded-lg' />
