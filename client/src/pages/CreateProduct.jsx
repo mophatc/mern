72d9ba61bf9f644d8bc3ea9f7f98d3eb183cb117
mophatc <mophatchemiati@gmail.com>
2024-09-13 15:02:53 +0300

    Image upload functionality added

--- client/src/pages/CreateProduct.jsx
+++ client/src/pages/CreateProduct.jsx
@@ -1,6 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import {getStorage,  getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import {app} from '../firebase'
 
 export default function createProduct() {
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading]=useState(false);
+  const [imageUploadError, setImageUploadError] = useState(false)
+  const [formData, setFormData] = useState({
+    imageUrls : []
+  });
+  console.log(formData)
+const handleUpload = ()=>{
+if (files.length >0 && files.length + formData.imageUrls.length<7){
+ 
+  const promises = [];
+  for(let i=0; i<files.length; i++){
+    promises.push(storeImage(files[i]))
+  }
+  Promise.all(promises).then((urls)=>{
+    setFormData({
+      ...formData, 
+      imageUrls:formData.imageUrls.concat(urls)
+    })
+    setImageUploadError(false)
+  }).catch((err)=>{
+    setImageUploadError('image upload failed (2mb max per image')
+  })
+
+ }else{
+  setImageUploadError(
+    'You can only upload 6 images per product '
+   
+  )
+ }
+
+}
+
+const storeImage = async (file)=>{
+ 
+  return new Promise ((resolve, reject)=>{
+    setUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime()+ file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef,file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot)=>{
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        if (progress===100){setUploading(false)}
+       
+
+      },
+      (error)=>{
+        reject(error)
+      
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+        resolve(downloadURL)
+      })
+    }
+
+    )
+  })
+
+  
+
+}
+
+const handleRemoveImage = (index)=>{
+  setFormData({
+    ...formData, 
+    imageUrls:formData.imageUrls.filter((_, i)=>
+      i!==index)
+  })
+}
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -86,7 +161,7 @@ export default function createProduct() {
                 min="1"
                 max="10"
               />
-              <div className="flex flex-col items-center" >
+              <div className="flex flex-col items-center">
                 <p>Regular Price</p>
                 <span className="text-xs">(Kshs./Month)</span>
               </div>
@@ -100,7 +175,7 @@ export default function createProduct() {
                 min="1"
                 max="10"
               />
-              <div className="flex flex-col items-center">
+              <div  className="flex flex-col  items-center">
                 <p>Discounted Price</p>
                 <span className="text-xs">(Kshs./Month)</span>
               </div>
@@ -108,15 +183,46 @@ export default function createProduct() {
           </div>
         </div>
         <div className="fle flex-1 ">
-          <p className="font-semibold">Images: 
+          <p className="font-semibold">
+            Images:
             <span className="font-normal text-gray-500 ml-2">
-              The first image will be the cover </span></p> 
-        <div className="flex gap-4">
-          <input type="file" className="p-3 border border-gray-300 rounded w-full" name="" id="images"  accept="images/*" multiple/>
-          <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+              The first image will be the cover (Max: 6 Images)
+            </span>
+          </p>
+          <div className="flex mt-3 gap-4">
+            <input
+              type="file"
+              className="p-3 border border-gray-300 rounded w-full"
+              name=""
+              onChange={(e)=>{ setFiles(e.target.files)}}
+              id="images"
+              accept="images/*"
+              multiple
+            />
+            <button disabled={uploading} type="button" onClick={handleUpload} className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              {uploading ? 'ULOADING...' : 'upload'}
+
+              
+            </button>
+          </div>
+          <p className="text-red-700 text-center capitalize">{imageUploadError && imageUploadError} </p>
+          {
+            formData.imageUrls.length>0&& formData.imageUrls.map((url, index)=>(
+              <div key={url} className="flex items-center justify-between border">
+
+                <img src={url} alt="product_image" className="w-20 h-20 object-contain rounded-lg mt-3" />
+                <button type="button" onClick={()=>handleRemoveImage(index)} className=" p-2 text-red-600 font-semibold hover:opacity-80 uppercase"> Delete</button>
+                
+                
+              </div>
+            ))
+          }
+          <button className="uppercase w-full mt-3 p-3 bg-slate-700 text-white rounded-lg hover:opacity-95 disabled:opacity-80">
+            Create Product{" "}
+          </button>
+          
         </div>
-        <button className="uppercase w-full mt-3 p-3 bg-slate-700 text-white rounded-lg hover:opacity-95 disabled:opacity-80">Create Product </button>
-        </div>
+        
       </form>
     </main>
   );
